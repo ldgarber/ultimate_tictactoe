7631c886b44ed94ccc9432689d23c47dbdff62fc
Leah Garber <leah.garber88@gmail.com>
2019-01-14 14:34:42 -0500

    using readline-sync and grabbing user names, printing board

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 const chalk = require('chalk'); 
 const clear = require('clear'); 
 const figlet = require('figlet'); 
+const readline = require('readline-sync'); 
+
+printBoard = (board) => {
+  printedBoard = 
+`${board[0]} | ${board[1]} | ${board[2]}
+---------
+${board[3]} | ${board[4]} | ${board[5]}
+---------
+${board[6]} | ${board[7]} | ${board[8]}`
+
+  console.log(printedBoard); 
+}
+
+
+clear(); 
+console.log(
+  chalk.yellow(
+    figlet.textSync('Tic Tac Toe', { horizontalLayout: 'full' })
+  )
+); 
+//starting the game: print empty board
+var game = {
+  board: [" "," "," "," "," "," "," "," "," "],  
+} 
+
+var playerOne = {
+  symbol: "X", 
+  name: ""
+} 
+var playerTwo = {
+  symbol: "O", 
+  name: ""
+}
+
+playerOne.name = readline.question("Player One, what is your name? "); 
+playerTwo.name = readline.question("Player Two, what is your name? "); 
+
+var currentPlayer = playerOne; 
+console.log("Your turn, " + playerOne.name); 
+printBoard(game.board); 
